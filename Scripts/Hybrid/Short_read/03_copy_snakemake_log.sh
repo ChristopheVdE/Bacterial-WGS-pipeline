b3e5ba8d216491ed00def21cbfb11ed70dde96a8
ChristopheVdE <46569386+ChristopheVdE@users.noreply.github.com>
2019-06-04 12:22:21 +0200

    Update 03_copy_snakemake_log.sh

--- Scripts/Hybrid/Short_read/03_copy_snakemake_log.sh
+++ Scripts/Hybrid/Short_read/03_copy_snakemake_log.sh
@@ -13,7 +13,7 @@ run="RUN_"`date +%Y%m%d`
 
 # COPY SNAKEMAKE LOG----------------------------------------------------------------------------------------
 echo -e "\nCopying snakemake log, please wait"
-mkdir -p /home/Pipeline/Hybrid/Short_reads/Snakemake_logs/
-cp -vr /home/Scripts/Hybrid/Short_read/.snakemake/log/* /home/Pipeline/Hybrid/Short_reads/Snakemake_logs/ 
+mkdir -p /home/Pipeline/Hybrid/Snakemake_logs/Short_reads/
+cp -vr /home/Scripts/Hybrid/Short_read/.snakemake/log/* /home/Pipeline/Hybrid/Snakemake_logs/Short_reads/
 echo -e "Done\n"
 #-----------------------------------------------------------------------------------------------------------
