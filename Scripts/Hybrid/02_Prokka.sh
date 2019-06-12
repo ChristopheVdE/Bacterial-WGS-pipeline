cef26cf857dca36cf393357e6af3f3403ea31987
ChristopheVdE <46569386+ChristopheVdE@users.noreply.github.com>
2019-06-12 12:59:39 +0200

    prokka hybrid

---
+++ Scripts/Hybrid/02_Prokka.sh
@@ -0,0 +1,46 @@
+###########################################################################################################
+#NAME SCRIPT: 02_Prokka.sh
+#AUTHOR: Christophe Van den Eynde  
+#Hybrid assembly
+#USAGE: ./02_Prokka.sh
+###########################################################################################################
+
+#FUNCTION==================================================================================================
+usage() {
+	errorcode=" \nERROR -> This script needs 2 parameters:\n
+		1: Path to folder where results should be saved
+        2: [OPTIONAL] Genus of organism
+        3: [OPTIONAL] Speciesof organism
+        4: [OPTIONAL] Kingdom that the organism belongs to
+        5: [OPTIONAL] Gram + or - (only for bacteria)
+        6: [OPTIONAL] The assembly to annotate
+        7: [OPTIONAL] Ammount of threads to use (default = 1)\n"
+	echo ${errorcode};
+	exit 1;
+}
+if [ "$#" -lt 2 ]; then
+	usage
+fi
+echo
+#==========================================================================================================
+
+#VARIABLES=================================================================================================
+Results=$1
+genus=$2
+species=$3
+kingdom=$4
+gram=$5
+assembly=$6
+threads=${7:-"1"}
+#==========================================================================================================
+
+#PROKKA====================================================================================================
+prokka \
+    --outdir ${Results} \
+    --genus ${genus} \
+    --species ${species} \
+    --kingdom ${kingdom} \
+    --gram ${gram} \
+    --cpus ${threads} \
+    ${assembly}
+#==========================================================================================================
