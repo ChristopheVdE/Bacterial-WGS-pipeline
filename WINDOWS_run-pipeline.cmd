24b5d9e34563cf242bd792ffb04705e1c6b7a8da
ChristopheVdE <46569386+ChristopheVdE@users.noreply.github.com>
2019-06-18 19:49:52 +0200

    script split - step1

--- WINDOWS_run-pipeline.cmd
+++ WINDOWS_run-pipeline.cmd
@@ -1,7 +1,16 @@
 @echo off
+REM INFO -----------------------------------------------------------
+echo ANALYSIS TYPE
+echo  - For short read assembly, input: '1' or 'short'
+echo  - For long read assembly, input: '2' or 'long'
+echo  - For hybrid assembly, input: '3' or 'hybrid'
 
-REM RUN SNAKEMAKE.txt-----------------------------------------------
-python.exe run_assembly.py
+REM ASK ANALYSIS TYPE-----------------------------------------------
+set /p analysis="Please provide Analysis type here: " && echo.
+REM ----------------------------------------------------------------
+
+REM START CORRECT ASSEMBLY WRAPPER----------------------------------
+python.exe run_assembly.py %analysis%
 REM ----------------------------------------------------------------
 
 PAUSE
