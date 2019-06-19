55a32bc72a3801e342ce6233340dea0fe272ff04
ChristopheVdE <46569386+ChristopheVdE@users.noreply.github.com>
2019-06-19 10:35:25 +0200

    script split - step 4

--- LINUX_run-pipeline.sh
+++ LINUX_run-pipeline.sh
@@ -15,30 +15,33 @@ echo "ANALYSIS TYPE"
 echo  "- For short read assembly, input: '1' or 'short'"
 echo  "- For long read assembly, input: '2' or 'long'"
 echo  "- For hybrid assembly, input: '3' or 'hybrid'"
-read -p "Please provide Analysis type here: " analysis 
-echo "\n"
+read -p "Please provide Analysis type here: " analysis
+echo $analysis
 #-----------------------------------------------------------------
 
 # GO TO REAL LOCATION OF SCRIPT (REQUIRED FOR MAC)----------------
 cd "$(dirname "$BASH_SOURCE")"
 
 # SHORT READ WRAPPER----------------------------------------------
-if ${analysis}==1 or ${analysis}==short
-    then
-        echo [SHORT] Starting Short reads assembly
-        echo.
-        python3 short_read_assembly.py
+if [ ${analysis} == 1 ]; then
+    echo "[SHORT] Starting Short reads assembly"
+    python3 ./short_read_assembly.py
+elif [ ${analysis} == 'short' ]; then
+    echo "[SHORT] Starting Short reads assembly"
+    python3 ./short_read_assembly.py
 # LONG READ ASSEMBLY-----------------------------------------------
-elif ${analysis}==2 or ${analysis}==long;
-    then
-        echo [LONG] Starting Long reads assembly
-        echo.
-        python3 long_read_assembly.py
+elif [ ${analysis} == 2 ]; then
+    echo "[LONG] Starting Long reads assembly"
+    python3 ./long_read_assembly.py
+elif [ ${analysis} == 'long' ]; then
+    echo "[LONG] Starting Long reads assembly"
+    python3 ./long_read_assembly.py
 # HYBRID ASSEMBLY---------------------------------------------------
-elif ${analysis}==3 or ${analysis}==hybrid
-    then
-        echo [HYBRID] Starting Hybrid assembly
-        echo.
-        python3 hybrid_assembly.py
+elif [ ${analysis} == 3 ]; then
+    echo "[HYBRID] Starting Hybrid assembly"
+    python3 ./hybrid_assembly.py
+elif [ ${analysis} == 'hybrid' ]; then
+    echo "[HYBRID] Starting Hybrid assembly"
+    python3 ./hybrid_assembly.py
 fi
 # ------------------------------------------------------------------
